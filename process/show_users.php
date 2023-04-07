e1dcb4d9c435ca6e1a1ac3abad1ed3ee65c4f3f4
Pugpaprika21 <pugpaprika21@gmail.com>
2023-04-07 07:42:11 -0700

    E

--- process/show_users.php
+++ process/show_users.php
@@ -5,7 +5,7 @@ require __DIR__ . '../../src/include/include.php';
 $data = $request['get'];
 
 if ($data['action'] == 'get_users') {
-    $users = db_select("user_tb", "*");
+    $users = db_excQuery("SELECT * FROM user_tb ORDER BY user_id DESC");
     echo json_encode($users);
     exit;
 }
