b0b13b1cfc9fe02fa6e44dd0165ecb026cba49bf
Pugpaprika21 <pugpaprika21@gmail.com>
2023-04-07 21:18:58 -0700

    e

--- process/show_users.php
+++ process/show_users.php
@@ -8,4 +8,24 @@ if ($data['action'] == 'get_users') {
     $users = db_excQuery("SELECT * FROM user_tb ORDER BY user_id DESC");
     echo json_encode($users);
     exit;
+    // $query_meetroom = db_excQuery(
+    //     "SELECT *
+    //         FROM rooms
+    //         WHERE room_id NOT IN (
+    //         SELECT room_id
+    //         FROM reservations
+    //         WHERE (
+    //             (start_datetime >= '2023-04-07 09:00:00' AND end_datetime <= '2023-04-07 10:00:00')
+    //             OR (start_datetime <= '2023-04-07 09:00:00' AND end_datetime >= '2023-04-07 10:00:00')
+    //             OR (start_datetime <= '2023-04-07 09:00:00' AND end_datetime >= '2023-04-07 09:00:00')
+    //             OR (start_datetime <= '2023-04-07 10:00:00' AND end_datetime >= '2023-04-07 10:00:00')
+    //         ) AND date = '2023-04-07'
+    //         ) AND room_type = 'standard'
+    //     "
+
+    // ,function ($resQuery) {
+
+    // });
+
+
 }
